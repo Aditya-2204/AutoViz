8a75e068b63e6664c393ecc942de0d2f8ebb18c2
Aditya-2204 <william1binki@gmail.com>
2025-04-28 09:17:33 -0500

    Build

---
+++ build/Desktop_x86_darwin_generic_mach_o_64bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,2 @@
+set(__QT_DEPLOY_TARGET_AutoViz_FILE /Users/adityachakraborty/Desktop/Code/C++/Robotics/AutoViz/build/Desktop_x86_darwin_generic_mach_o_64bit-Debug/AutoViz.app/Contents/MacOS/AutoViz)
+set(__QT_DEPLOY_TARGET_AutoViz_TYPE EXECUTABLE)
---
+++ build/Desktop_x86_darwin_generic_mach_o_64bit-Debug/CMakeFiles/AutoViz.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "AutoViz_autogen"
+  "CMakeFiles/AutoViz_autogen.dir/AutogenUsed.txt"
+  "CMakeFiles/AutoViz_autogen.dir/ParseCache.txt"
+  "AutoViz.app/Contents/MacOS/AutoViz"
+  "AutoViz.pdb"
+  "CMakeFiles/AutoViz.dir/AutoViz_autogen/mocs_compilation.cpp.o"
+  "CMakeFiles/AutoViz.dir/AutoViz_autogen/mocs_compilation.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AutoViz.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
